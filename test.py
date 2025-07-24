ff577edee50698be1b54f818bf90f73abf1eb44f
irishwerr <irishwerr@gmail.com>
2025-07-24 02:25:35 -0400

    test.py changed

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 import google.generativeai as genai
 
 # Set your API key
-<<<<<<< HEAD
-genai.configure(api_key="AIzaSyD_CSx_yBaWq4iNfJmYUUI0LxGVl5MOq_4")  # Or use os.getenv("GOOGLE_API_KEY")
-=======
-genai.configure(api_key="api")  # Or use os.getenv("GOOGLE_API_KEY")
->>>>>>> 89d9e9afc74571784ddbc16ce4e2aa624c1f6fe5
+genai.configure(api_key=os.getenv("API_KEY"))  # Or use os.getenv("GOOGLE_API_KEY")
 
 # List all models
 models = genai.list_models()
